99f66fad2fd386360e77e6511e31fbebdf0e240f
Pooja <poojabacchewar123@gmail.com>
2019-09-15 20:22:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/toggleb/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.toggleb;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    ToggleButton toggleButton;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toggleButton = findViewById(R.id.toggleButton);
+        mediaPlayer=MediaPlayer.create(MainActivity.this,R.raw.akk);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
+                   mediaPlayer.start();
+                }else
+                {
+                    mediaPlayer.pause();
+                }
+
+            }
+
+        });
+    }
+}
